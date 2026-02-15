3d6c95e1912bcfbe9fba66c684023759ac9a739c
mrGoonies <gooniesdev@gmail.com>
2026-02-15 00:27:06 -0300

    Agregando campo de categoria ticket

--- tickets/models.py
+++ tickets/models.py
@@ -6,6 +6,7 @@ from datetime import datetime
 class Ticket(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=250)
+    category = models.CharField(choices=(("1", "Hardware"), ("2", "Software"), ("3", "Redes"), ("4", "Otro")), default="1", max_length=30)
     status = models.CharField(choices=(("1", "Pendiente"), ("2", "En Desarrollo"), ("3", "Finalizado")), default="1", max_length=30)
     contact_info = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
